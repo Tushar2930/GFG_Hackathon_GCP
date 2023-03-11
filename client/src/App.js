6b2c031ae88877e3c920c640e9332db5bbd8cc90
just-ctrlC-ctrlV <109028862+just-ctrlC-ctrlV@users.noreply.github.com>
2023-03-11 10:34:00 +0530

    new-folder-structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
-import {Routes,Route, json,useParams} from "react-router-dom";
-import Home from "./Components/Home"
-import Navbar from "./Components/Navbar"
-import './App.css';
-import About from "./Components/AboutUs.js"
-import Contact from "./Components/Contact.js"
-import Blog from "./Components/Blog.js"
-import Shop from "./Components/Shop.js"
+import { Routes, Route, json, useParams } from "react-router-dom";
+import Home from "./Components/Content/Home/Home.js";
+import Navbar from "./Components/Navbar/Navbar.js";
+import "./App.css";
+import About from "./Components/Content/About/AboutUs.js";
+import Contact from "./Components/Content/Contact/Contact.js";
+import Blog from "./Components/Content/Blog/Blog.js";
+import Shop from "./Components/Content/Shop/Shop.js";
 
 function App() {
   return (
     <div className="App">
-    <Navbar/>
+      <Navbar />
       <Routes>
-     <Route exact path="/" element={<Home/>} />
-     <Route exact path="/about-us" element={<About/>} />
-     <Route exact path="/contact-us" element={<Contact/>} />
-     <Route exact path="/blog" element={<Blog/>} />
-     <Route exact path="/shop" element={<Shop/>} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about-us" element={<About />} />
+        <Route exact path="/contact-us" element={<Contact />} />
+        <Route exact path="/blog" element={<Blog />} />
+        <Route exact path="/shop" element={<Shop />} />
       </Routes>
     </div>
   );
---
+++ client/src/Components/Content/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Carousel from "./Carousel/Carousel";
+import Featured from "./Features/Featured";
+
+function Home() {
+  return (
+    <>
+      <Carousel />
+      <Featured />
+    </>
+  );
+}
+
+export default Home;
