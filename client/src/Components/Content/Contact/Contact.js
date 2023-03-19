528eafe488a9dd09a08eb7f8d866b968605a9450
just-ctrlC-ctrlV <109028862+just-ctrlC-ctrlV@users.noreply.github.com>
2023-03-19 11:47:57 +0530

    contact-form

--- client/src/Components/Content/Contact/Contact.js
+++ client/src/Components/Content/Contact/Contact.js
@@ -1,11 +1,13 @@
 import React from "react";
+import ContactForm from "./ContactForm";
 
-
-function Contact(){
-
-    return <>
-        Contact-us
+function Contact() {
+  return (
+    <>
+      Contact-us
+      <ContactForm></ContactForm>
     </>
+  );
 }
 
-export default Contact
+export default Contact;
