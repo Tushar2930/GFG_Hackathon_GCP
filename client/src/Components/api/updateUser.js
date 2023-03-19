681c03cfb51518c04971160199b0181868fbe2b1
just-ctrlC-ctrlV <109028862+just-ctrlC-ctrlV@users.noreply.github.com>
2023-03-20 00:27:58 +0530

    get/post/patch-user

---
+++ client/src/Components/api/updateUser.js
@@ -0,0 +1,19 @@
+export const updateUser = async (postData) => {
+  try {
+    const requestOptions = {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        status: "new user",
+        postData,
+      }),
+    };
+    const data = await fetch(
+      "http://localhost:8000/user/updateUser",
+      requestOptions
+    ).then((res) => res.json());
+    return data.data;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
