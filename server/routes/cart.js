a579f999bf2f8277b71386d935e089b3eefaa34e
just-ctrlC-ctrlV <109028862+just-ctrlC-ctrlV@users.noreply.github.com>
2023-03-22 18:28:30 +0530

    asas

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,9 +1,9 @@
-const express=require('express');
-const router=express.Router();
-const cartController=require("../controllers/cartController");
+const express = require("express");
+const router = express.Router();
+const cartController = require("../controllers/cartController");
 
-router.post('/add-product',cartController.add);
-router.post('/get-products',cartController.get);
+router.post("/add-product", cartController.add);
+router.post("/get-products", cartController.getCart);
+router.patch("/update-user-cart-item", cartController.updateUserCartItem);
 
-
-module.exports=router;
+module.exports = router;
