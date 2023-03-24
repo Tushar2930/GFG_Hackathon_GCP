e5b482759de1188282b8c5f1c692bb96a10adc7c
Aryan Bhargava <aryan.bhargava.mec21@itbhu.ac.in>
2023-03-24 23:10:50 +0530

    Contact

--- client/src/Components/Content/About/components/AboutComponent.js
+++ client/src/Components/Content/About/components/AboutComponent.js
@@ -3,10 +3,10 @@ import "./AboutComponent.css"
 import AboutCard from "./AboutCard";
 
 var aboutDataArray = [
-    {name:"Aryan", contact_num:"696969", description:"Pellentesque in ipsum id orci porta dapibus. Pellentesque in ipsum id orci porta dapibus. Pellentesque in ipsum id orci porta dapibus.", img_url:"https://glamournepal.com/wp-content/uploads/2023/01/Rocky-Bhai-Yash.jpg"},
-    {name:"Sumit", contact_num:"696969", description:"Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat. Donec rutrum congue leo eget malesuada. Cras ultricies ligula sed magna dictum porta.", img_url:"https://glamournepal.com/wp-content/uploads/2023/01/Rocky-Bhai-Yash.jpg"},
+    {name:"Sumit", contact_num:"696969", description:"Pellentesque in ipsum id orci porta dapibus. Pellentesque in ipsum id orci porta dapibus. Pellentesque in ipsum id orci porta dapibus.", img_url:"https://glamournepal.com/wp-content/uploads/2023/01/Rocky-Bhai-Yash.jpg"},
+    {name:"Tushar", contact_num:"696969", description:"Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat. Donec rutrum congue leo eget malesuada. Cras ultricies ligula sed magna dictum porta.", img_url:"https://glamournepal.com/wp-content/uploads/2023/01/Rocky-Bhai-Yash.jpg"},
     {name:"Tanmay", contact_num:"696969", description:"Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat. Donec rutrum congue leo eget malesuada. Cras ultricies ligula sed magna dictum porta.", img_url:"https://glamournepal.com/wp-content/uploads/2023/01/Rocky-Bhai-Yash.jpg"},
-    {name:"Tushar", contact_num:"696969", description:"Pellentesque in ipsum id orci porta dapibus. Pellentesque in ipsum id orci porta dapibus. Pellentesque in ipsum id orci porta dapibus.", img_url:"https://glamournepal.com/wp-content/uploads/2023/01/Rocky-Bhai-Yash.jpg"},
+    {name:"Aryan", contact_num:"696969", description:"Pellentesque in ipsum id orci porta dapibus. Pellentesque in ipsum id orci porta dapibus. Pellentesque in ipsum id orci porta dapibus.", img_url:"https://glamournepal.com/wp-content/uploads/2023/01/Rocky-Bhai-Yash.jpg"},
 ]
 
 function AboutComponent(){
