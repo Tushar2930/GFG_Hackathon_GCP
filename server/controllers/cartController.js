81e670692d5c03e324abc8c800727348f6d5d720
just-ctrlC-ctrlV <109028862+just-ctrlC-ctrlV@users.noreply.github.com>
2023-03-31 15:52:11 +0530

    landingPage

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -8,24 +8,23 @@ module.exports.add = async function (req, res) {
     var ref = await snapsh.where("email", "==", `${email}`).get();
     var temp = ref.docs[0];
     var array = await temp?.data()?.cartArray;
-    if(!array){
+    if (!array) {
       array = [];
     }
-    if(array.find((item) => item.id === req.body.id)){
+    if (array.find((item) => item.id === req.body.id)) {
       return res.status(200).json({
         message: "already added",
       });
     }
 
     array.push(req.body);
-    await db.collection("users").doc(temp.id).update({
+    await db.collection("users").doc(temp?.id).update({
       cartArray: array,
     });
     // console.log(array);
     res.status(200).json({
       message: "success",
     });
-    
   } catch (error) {
     console.log(error);
   }
@@ -40,7 +39,7 @@ module.exports.getCart = async function (req, res) {
 
     var temp = ref.docs[0];
     var array = await temp?.data()?.cartArray;
-    if(!array){
+    if (!array) {
       array = [];
     }
     // console.log(array);
@@ -48,7 +47,6 @@ module.exports.getCart = async function (req, res) {
       message: "success",
       cart: array,
     });
-
   } catch (error) {
     console.log(error);
   }
@@ -70,10 +68,9 @@ module.exports.updateUserCartItem = async function (req, res) {
   }
 };
 
-
 module.exports.getImage = async function (req, res) {
   try {
-   //get image from product doc.id
+    //get image from product doc.id
     const cart = req.body.cart;
     var temp = [];
     for (let i = 0; i < cart.length; i++) {
@@ -87,9 +84,7 @@ module.exports.getImage = async function (req, res) {
       message: "success",
       image: temp,
     });
-
   } catch (error) {
     console.log(error);
   }
-}
-
+};
