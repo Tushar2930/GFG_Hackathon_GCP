5f6ef0ed002d36658a33f0518663e92db6476596
Tushar <tusharshingane1234@gmail.com>
2023-04-04 17:13:28 +0530

    reset password

--- client/src/Components/Content/Signup/SignIn.js
+++ client/src/Components/Content/Signup/SignIn.js
@@ -99,7 +99,7 @@ export default function SignIn() {
                 <input type="checkbox" />
                 Remember me
               </div>
-              <div><a href="/signin/resetpassword">Forgot Password ?</a></div>
+              <div><Link to="/signin/resetpassword">Forgot Password ?</Link></div>
             </div>
             <button className="submit-btn" disabled={isLoading} onClick={() => handleSignIn()}>{!isLoading ? "Login" : <CircularProgress />}</button>
             <button className="submit-btn" disabled={isLoading}
