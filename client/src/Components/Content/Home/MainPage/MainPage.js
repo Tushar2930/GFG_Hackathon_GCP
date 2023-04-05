2436f0b017f6bc754539aad3156dbb85f1d26029
Tushar <tusharshingane1234@gmail.com>
2023-04-06 03:51:46 +0530

    done

--- client/src/Components/Content/Home/MainPage/MainPage.js
+++ client/src/Components/Content/Home/MainPage/MainPage.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import "./MainPage.css"
+import {Zoom,Fade} from "react-awesome-reveal"
 
 const MainPage = () => {
   return (
-    <div className='land-root'></div>
+    <div className='land-root py-96'>
+    <Zoom><Fade>
+    <h1 className="rounded-3xl relative px-4 py-4 w-1/4 text-center text-white" style={{backgroundColor:"#ffa900",fontWeight:"800",fontSize:"xxx-large",left:"39%"}}>AGROKART</h1>
+    </Fade>
+    </Zoom>
+    </div>
   )
 }
 
