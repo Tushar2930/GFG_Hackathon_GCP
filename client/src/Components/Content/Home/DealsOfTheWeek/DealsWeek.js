4b62cef9950bf40112261e402dcac9daa2fdb725
Tushar <tusharshingane1234@gmail.com>
2023-04-06 00:37:21 +0530

    frontend multiple

--- client/src/Components/Content/Home/DealsOfTheWeek/DealsWeek.js
+++ client/src/Components/Content/Home/DealsOfTheWeek/DealsWeek.js
@@ -1,31 +1,86 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "./dealsweek.css";
+import Clock from "./Clock";
+
+
 
 function DealsWeek() {
+  const [timerDays, setTimerDays] = useState();
+  const [timerHours, setTimerHours] = useState();
+  const [timerMinutes, setTimerMinutes] = useState();
+  const [timerSeconds, setTimerSeconds] = useState();
+
+  let interval;
+
+  const startTimer = () => {
+    const countDownDate = new Date("May 30,2023 ").getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+
+      const distance = countDownDate - now;
+
+      const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+      const hours = Math.floor(
+        (distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
+      const seconds = Math.floor((distance % (60 * 1000)) / 1000);
+      if (distance < 0) {
+        // Stop Timer
+
+        clearInterval(interval.current);
+      } else {
+        // Update Timer
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    });
+  };
+
+  useEffect(() => {
+    startTimer();
+  },[]);
+
   return (
     <>
       <div className="droot">
         <section class="text-gray-600 body-font w-screen">
           <div class=" px-5 py-24 mx-auto flex flex-wrap  w-screen">
-            <div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+            <div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden bg-white flex justify-center content-center" style={{alignItems:"center"}}>
               <img
+              className="img-fluid"
                 alt="feature"
-                class="object-cover object-center w-full"
-                src="https://dummyimage.com/460x500"
+                class="object-cover object-center h-5/6 w-5/6"
+                src="https://image.lexica.art/full_jpg/64a52a2f-79bf-4929-bc9d-9722fbfff126"
               />
             </div>
             <div class="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
               <div class="flex flex-col mb-10 lg:items-start items-center">
                 <div class="flex-grow">
-                  <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
-                    Shooting Stars
+                  <h2 class="text-orange-500 text-xl title-font font-medium mb-3 text-left">
+                    _________
                   </h2>
-                  <p class="leading-relaxed text-base">
+                  <h2 className="text-5xl text-left text-gray-900 py-4 hover:text-orange-600">
+                    Quisqiue Voluptas
+                  </h2>
+                  <h2 className="text-left">
+                    <span className="text-2xl text-left text-orange-600">₹ 999</span>
+                  </h2>
+                  <p class="leading-relaxed text-base w-3/4 py-4 text-left">
                     Blue bottle crucifix vinyl post-ironic four dollar toast
                     vegan taxidermy. Gastropub indxgo juice poutine.
                   </p>
-                  <a class="mt-3 text-indigo-500 inline-flex items-center">
-                    Learn More
+                  <Clock
+                  timerDays={timerDays}
+                  timerHours={timerHours}
+                  timerMinutes={timerMinutes}
+                  timerSeconds={timerSeconds}
+                  />
+                  <a class="mt-3 text-indigo-500 inline-flex items-center btn-cart">
+                    Add to Cart
                     <svg
                       fill="none"
                       stroke="currentColor"
--- client/src/Components/Content/Home/RentingSection/RentingCardList.js
+++ client/src/Components/Content/Home/RentingSection/RentingCardList.js
@@ -1,104 +1,34 @@
 import React, { useEffect, useState } from "react";
-import Card from "./RentingCard";
 import "./RentingCardList.css";
 import { Link } from "react-router-dom";
+import { Slide ,Fade, Zoom} from "react-awesome-reveal";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 //
 
 function RentingCardList() {
-  // const [data, setData] = useState([]);
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const data=await fetch("http://localhost:8000/rent/getAllFarmers",{
-  //       method:"GET",
-  //       headers:{
-  //         "Content-Type":"application/json"
-  //       }
-  //     });
-  //     const res=await data.json();
-  //     setData(res.data);
-  //     setData(res.data);
-  //   }
-  //   fetchData();
-  // }, []);
-
-  // var cardComponentArray = data.map(
-  //   (card,k) => {
-  //     if(k<4){
-  //     return  (
-  //       <div class="col-6 px-4 mb-4">
-  //         <Card name={card.name} content={card.service} address={card.Address} area={card.area} date={card.date} duration={card.duration} price={card.price}/>
-  //       </div>
-  //     )
-  //     }
-  //   }
-  // )
 
   return (
     <div class=" main_fluid-container">
-      <div className="row div-container flex flex-coloum">
+      <div className="row div-container flex flex-col">
         <div class="font flex">
-          <b>Rental Services</b>
+        <Slide className="px-4 py-4">
+          <b className="text-5xl  h-1/4 font-temp pt-16 pb-0 flex" style={{color:"#ffb300"}}><p className="text-black px-4">Rental </p> Services</b></Slide>
         </div>
-        <p className="w-1/2 text-white flex">
-          n publishing and graphic design, Lorem ipsum is a placeholder text
-          commonly used to demonstrate the visual form of a document or a
-          typeface without relying on meaningful content. Lorem ipsum may be
-          used as a placeholder before final copy is available
-        </p>
-        {/* {cardComponentArray} */}
-        <button type="button" class="w-36 h-10 flex">
+        
+        <Slide direction="right"><Fade direction="up"><p className="w-1/2 text-black flex my-12 text-lg " style={{fontFamily:"cursive"}}>
+        Find quality farm equipment for your operation with our rental service.<br/>
+         From tractors to tillers, we offer a variety of options to fit your needs. 
+         Convenient, reliable, and hassle-free rentals for a stress-free farming experience.
+        </p></Fade></Slide>
+        <Zoom>
+        <button type="button" class="flex justify-center content-center h-12 w-48 rounded-3xl" style={{backgroundColor:"#4d8087"}}>
           <Link className="view-more-link" to="/rent">
             Know More
             <ArrowForwardIosIcon />
           </Link>
-        </button>
+        </button></Zoom>
       </div>
-      {/* <section class="text-gray-600 body-font">
-        <div
-          class="container px-5 py-24 mx-auto "
-          style={{ width: "100vw", background: "none", boxShadow: "none" }}>
-          <div class="flex flex-wrap -m-4 ">
-            <div class="lg:w-1/4 md:w-3/4 p-4 w-full">
-              <a class="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  class="object-cover object-center w-full h-full block"
-                  src="https://dummyimage.com/420x260"
-                />
-              </a>
-              <div class="mt-4">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">
-                  The Catalyzer
-                </h2>
-                <p class="mt-1">$16.00</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-3/4 p-4 w-full">
-              <a class="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  class="object-cover object-center w-full h-full block"
-                  src="https://dummyimage.com/420x260"
-                />
-              </a>
-              <div class="mt-4">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">
-                  The Catalyzer
-                </h2>
-                <p class="mt-1">$16.00</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section> */}
     </div>
   );
 }
