9b1e3a5516b2bec8f7f1892b5fe825ab0cb7a559
just-ctrlC-ctrlV <109028862+just-ctrlC-ctrlV@users.noreply.github.com>
2023-04-09 00:15:41 +0530

    user_profile

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,7 +6,6 @@ router
   .post("/create", userController.create)
   .post("/getUser", userController.getUser)
   .post("/get-user-products", userController.getUserProducts)
-  .post("/get-user-service_requests", userController.getUserServiceRequest)
   .post("/get-user-rent", userController.getUserRent)
   .patch("/updateUser", userController.updateUser);
 
