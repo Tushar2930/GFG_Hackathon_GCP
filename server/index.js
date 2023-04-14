e5de69723d430dbc909cc8fa1fcf9f9fc29ee4d3
Sumit <sumitscience2017@gmail.com>
2023-04-14 15:58:53 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -16,3 +16,26 @@ const PORT=8000;
 app.listen(PORT,()=>{
     console.log(`Server is running on port : ${PORT} `);
 })
+
+
+
+
+
+
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/agrokart/client;
+
+    index index.html index.htm index.nginx-debian.html;
+    server_name_;
+
+    location / {
+    proxy_pass http://localhost:8000;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host $host;
+    proxy_cache_bypass $http_upgrade;
+    }
+}
