3f1396c9178b006875e6aa2b93a2439165a296d7
Sumit <sumitscience2017@gmail.com>
2023-04-15 01:25:58 +0530

    favicon added

--- server/index.js
+++ server/index.js
@@ -3,10 +3,6 @@ const bodyParser=require("body-parser");
 const cors=require("cors");
 const firebase_admin=require("firebase-admin");
 const app=express();
-const path=require('path');
-
-
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
@@ -22,24 +18,3 @@ app.listen(PORT,()=>{
 })
 
 
-
-
-
-
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/agrokart/client;
-
-    index index.html index.htm index.nginx-debian.html;
-    server_name_;
-
-    location / {
-    proxy_pass http://localhost:8000;
-    proxy_http_version 1.1;
-    proxy_set_header Upgrade $http_upgrade;
-    proxy_set_header Connection 'upgrade';
-    proxy_set_header Host $host;
-    proxy_cache_bypass $http_upgrade;
-    }
-}
