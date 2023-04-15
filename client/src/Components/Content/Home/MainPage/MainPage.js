59bdda1da543f02a8146fed7e78319df60e0a5c5
just-ctrlC-ctrlV <109028862+just-ctrlC-ctrlV@users.noreply.github.com>
2023-04-16 01:19:27 +0530

    tuning_3.0

--- client/src/Components/Content/Home/MainPage/MainPage.js
+++ client/src/Components/Content/Home/MainPage/MainPage.js
@@ -5,25 +5,35 @@ import { Slide } from "react-awesome-reveal";
 import "./MainPage.css";
 
 const MainPage = () => {
-  return <motion.div className="land-root py-20 sm:py-96 ">
-  <div className="data-root">
-    <div className="data-head"><Slide>Welcome to </Slide><div className="flex bsdk"><Slide direction="up">Agro</Slide><div style={{color:"black"}}><Slide direction="down">Kart</Slide></div></div></div>
-    {/* <div className="data-moredata">We are certified Agricultural service.
-    This is one of the best platform for farmers to sell their products and for buyers to buy products directly from farmers. */}
-    <div className="data-moredata">
-    <Typewriter
-    onInit={(typewriter)=> {
-      typewriter
-      .typeString("We are certified Agro-service company . This is one of the best platform for farmers to sell their products and for buyers to buy products directly from farmers.")
-      .changeDelay(50)
-      .start();
-  }}
-  />
-    </div>
-    <div className="butn">Connect with us</div>
-  </div>
-  </motion.div>;
+  return (
+    <motion.div className="land-root py-20 sm:py-96 ">
+      <div className="data-root sm:bottom-48">
+        <div className="data-head mt-16  text-xl sm:text-6xl">
+          <Slide>Welcome to </Slide>
+          <div className="flex text-3xl sm:text-8xl bsdk">
+            <Slide direction="up">Agro</Slide>
+            <div style={{ color: "black" }}>
+              <Slide direction="down">Kart</Slide>
+            </div>
+          </div>
+        </div>
+
+        <div className="data-moredata text-xs sm:text-xl  sm:flex">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString(
+                  "We are certified Agro-service company . This is one of the best platform for farmers to sell their products and for buyers to buy products directly from farmers."
+                )
+                .changeDelay(50)
+                .start();
+            }}
+          />
+        </div>
+        <div className="butn hidden sm:flex">Connect with us</div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default MainPage;
- 
