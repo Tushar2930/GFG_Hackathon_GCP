15ac57656e307b605ef778c925a903d5e7ee7aac
Tushar2930 <110845672+Tushar2930@users.noreply.github.com>
2023-06-11 12:07:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use('/',require('./routes'));
  
-const PORT=8000;
+const PORT=process.env.PORT | 8000;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port : ${PORT} `);
